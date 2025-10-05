55dcde0ac7497809c734ae05f4f5e565768b1ef2
Mihir Patel <mihirpatel.mp.1112@gmail.com>
2025-10-05 12:40:21 +1100

    fix

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import DashboardGrid from "@/components/dashboardgrid";
+import DashboardGrid from "@/components/dashgrid";
 import AQSummary from "@/components/aqSummary";
 
 export default function Dashboard() {
